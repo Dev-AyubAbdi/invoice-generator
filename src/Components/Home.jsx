3e17a5e0a2711a724f0a73fd30ec425c5d795b34
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2025-12-12 14:52:35 +0300

    added section hero

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
 export const Home = () => {
-    const today = new Date().toISOString().split("T")[0];
-    const [invoiceNumber, setInvoiceNumber] = useState(1)
+  const today = new Date().toISOString().split("T")[0];
+  const [invoiceNumber, setInvoiceNumber] = useState(1);
+  const [cashierName, setCashierName] = useState("");
+  const [costomerName, setCustomerName] = useState("");
   return (
     <div className="max-w-7xl mx-auto pt-10">
       <div className="flex">
@@ -11,14 +13,41 @@ export const Home = () => {
           {/* Top section */}
           <div className="flex justify-between pb-2 border-b border-gray-200">
             <h2>Current Date: {today}</h2>
-           <div className="flex items-center">
-             <h3 className="pr-3">Invoice Number:</h3>
-             <input type="number" className=" outline-0 p-2 bg-gray-100 rounded-lg" value={invoiceNumber} onChange={(e)=> setInvoiceNumber(e.target.value)} />
-           </div>
+            <div className="flex items-center">
+              <h3 className="pr-3">Invoice Number:</h3>
+              <input
+                type="number"
+                className=" outline-0 p-2 bg-gray-100 rounded-lg"
+                value={invoiceNumber}
+                onChange={(e) => setInvoiceNumber(e.target.value)}
+              />
+            </div>
           </div>
           {/* hero section */}
           <div>
-            <h2>Invoice</h2>
+            <h2 className="text-center font-bold">INVOICE</h2>
+            <div className="flex justify-around">
+              <div className="">
+                <h2>Cashier:</h2>
+                <input
+                  type="text"
+                  className="bg-gray-100 rounded p-2 w-130 outline-0 text-lg"
+                  placeholder="Cashier Name"
+                  value={cashierName}
+                  onChange={(e) => setCashierName(e.target.value)}
+                />
+              </div>
+              <div>
+                <h2>Customer:</h2>
+                <input
+                  type="text"
+                  className="bg-gray-100 rounded p-2 w-130 outline-0 text-lg"
+                  placeholder="Customer Name"
+                  value={costomerName}
+                  onChange={(e) => setCustomerName(e.target.value)}
+                />
+              </div>
+            </div>
           </div>
         </div>
         {/* Left side */}
